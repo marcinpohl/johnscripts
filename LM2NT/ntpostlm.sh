02d5d30728de87bf489c0c6b29cc61efa6a8409b
Marcin Pohl <marcinpohl@gmail.com>
2011-06-04 13:52:18 -0400

    moved ntpostlm to new selfcontained dir chopped it up into functions for easier debug introduced incremental rockyou based charset want to remove old ntpostlm.sh

---
+++ LM2NT/ntpostlm.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+set -x
+set -e
+shopt -u dotglob         
+shopt -s extglob         
+shopt -u failglob       
+shopt -u globstar      
+shopt -u nocaseglob   
+shopt -u nullglob    
+
+JTRBASE="/home/marcin/hashes/crackall.git"
+JTR="${JTRBASE}/john-1.7.7-jumbo-5/run/john"
+JTRconfigLM="${JTRBASE}/john.conf"
+JTRconfigNT="${JTRBASE}/john-ntpostlm.conf"
+PASSWD="${JTRBASE}/crackme"
+OUTPUTDIR="${JTRBASE}/johnscripts/LM2NT"
+
+DEBUG=0  #0 slow, 1 fast
+DESTRUCT=1  #0 destructive, 1 leaves the old files alone
+
+#pushd $(pwd)
+#cd ${OUTPUTDIR}
+
+function phase1reallyquick {
+    echo "*** Starting to do the really quick dictionaries"
+	   DICTS1="${JTRBASE}/dicts/Really_Quick/*"
+	   for d in $DICTS1; do
+		  $JTR \
+			 --config=${JTRconfigLM} \
+			 --session=LM_phase1 \
+			 --pot=LM_phase1.pot \
+			 --w:"$d" --rules \
+			 --format=lm \
+			 $PASSWD
+			 done
+}
+
+function phase1quicklists {
+### BIGGER DICTIONARIES, still should be fast, debug only
+echo "*** Starting to do the Quick Lists"
+   DICTS2=$(ls -1 dicts/Quick_Lists/*)
+   for d in $DICTS2; do
+	    $JTR \
+		  --config=${JTRconfigLM} \
+		  --session=LM_phase1 \
+		  --pot=LM_phase1 \
+		  --w:"$d" \
+		  --format=lm \
+		  $PASSWD
+    done
+}
+
+function phase1largelists {
+echo "*** Starting to do the Large Lists"
+#DICTS2=$(ls -1 dicts/Large_Lists/*)
+DICTS2=$(find dicts/Large_Lists/ -type f -maxdepth 1)
+for d in $DICTS2; do
+	$JTR \
+		--config=${JTRconfigLM} \
+		--session=LM_phase1 \
+		--pot=LM_phase1 \
+		--w:"$d" \
+		--format=lm \
+		$PASSWD
+done
+}
+
+function phase1bruteforce {
+### FOR BRUTEFORCING THE REST
+echo "*** Starting to do BRUTE FORCE"
+echo "*** Hit CTRL-C after 20mins or whenever the passwords stop spewing"
+$JTR \
+   --config=$JTRconfigLM \
+   --session=LM_phase1 \
+   --pot=LM_phase1 \
+   --format=LM \
+   $PASSWD
+}
+
+function phase1incremental {
+echo "*** Starting to do INCREMENTAL-ROCKYOULANMAN "
+$JTR \
+	--incremental=RockYouLanMan \
+	--session=LM_phase1 \
+	--pot=LM_phase1.pot \
+	--format=LM \
+	$PASSWD
+}
+
+function phase2 {
+### extraction of cracked LM
+echo "*** Extracting the cracked LM hashes"
+$JTR -format=lm -show -pot=./LM_phase1.pot $PASSWD | \
+grep -v "password hashes cracked" | \
+sort -u | \
+cut -d: -f2 > LMcracked
+}
+
+function phase3 {
+### using the cracked LM list as wordlist, with different rules
+echo "*** Starting to crack NT only for passwords with cracked LM"
+$JTR \
+--config=${JTRconfigNT} \
+--session=./NT_phase3 \
+--pot=./LM_phase1.pot \
+--w=./LMcracked --rules \
+--format=nt \
+$PASSWD
+
+# output some stats
+echo -n "LanMan stats: "
+$JTR -show --pot=./LM_phase1.pot --format=lm $PASSWD | tail -n1
+echo -n "NTLM stats: "
+$JTR -show --pot=./LM_phase1.pot --format=nt $PASSWD | tail -n1
+}
+
+### script actually starts to run here
+if [ $DESTRUCT -eq 0 ]
+then
+if [ -f LM_phase1 ]; then rm LM_phase1* ; fi
+if [ -f LM_phase1 ]; then rm NT_phase3* ; fi
+if [ -f LM_phase1 ]; then rm LMcracked ; fi
+fi
+
+phase1reallyquick
+#phase1quicklists
+#phase1largelists
+#phase1bruteforce
+phase1incremental
+phase2
+phase3
+
--- ntpostlm.sh
+++ ntpostlm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-set -e
+set +e
 shopt -u dotglob         
 shopt -s extglob         
 shopt -u failglob       
@@ -8,61 +8,91 @@ shopt -u globstar
 shopt -u nocaseglob   
 shopt -u nullglob    
 
-JTR=~/hashes/crackall.git/jtr/john
-JTRconfigLM=~/hashes/crackall.git/jtr/john.conf
-JTRconfigNT=~/hashes/crackall.git/jtr/john-ntpostlm.conf
-PASSWD=~/hashes/crackall.git/crackme
+JTRBASE="~/hashes/crackall.git"
+JTR="${JTRBASE}/john-1.7.7-jumbo-5/run/john"
+JTRconfigLM="${JTRBASE}/john.conf"
+JTRconfigNT="${JTRBASE}/john-ntpostlm.conf"
+PASSWD="${JTRBASE}/crackme"
+OUTPUTDIR="${JTRBASE}/LM2NT"
 
-DEBUG=0  #0 slow, 1 fast
+DEBUG=1  #0 slow, 1 fast
+DESTRUCT=1  #0 destructive, 1 leaves the old files alone
 
 pushd $(pwd)
-cd ~marcin/hashes/crackall.git
-if [ -f LM_phase1 ]; then rm LM_phase1* ; fi
-if [ -f LM_phase1 ]; then rm LNT_phase1* ; fi
-if [ -f LM_phase1 ]; then rm LMcracked ; fi
+cd ${JTRBASE}
 
-DICTS1=$(ls -1 dicts/Really_Quick/*)
+if [ $DESTRUCT -eq 0 ]
+then
+    if [ -f LM_phase1 ]; then rm LM_phase1* ; fi
+    if [ -f LM_phase1 ]; then rm LNT_phase1* ; fi
+    if [ -f LM_phase1 ]; then rm LMcracked ; fi
+fi
+
+echo "*** Starting to do the really quick dictionaries"
+DICTS1="${JTRBASE}/dicts/Really_Quick/*"
 for d in $DICTS1; do
 	$JTR \
 		--config=${JTRconfigLM} \
 		--session=LM_phase1 \
 		--pot=LM_phase1 \
-		--w:$d --rules \
+		--w:"$d" --rules \
 		--format=lm \
 		$PASSWD
 done
 
 ### BIGGER DICTIONARIES, still should be fast, debug only
-if [ DEBUG -eq 0]; 
+if [ $DEBUG -eq 0 ] 
 then
-    DICTS2=$(ls -1 dicts/Quick_Lists/*)
+#	echo "*** Starting to do the Quick Lists"
+#    DICTS2=$(ls -1 dicts/Quick_Lists/*)
+#    for d in $DICTS2; do
+#	    $JTR \
+#		  --config=${JTRconfigLM} \
+#		  --session=LM_phase1 \
+#		  --pot=LM_phase1 \
+#		  --w:"$d" \
+#		  --format=lm \
+#		  $PASSWD
+#    done
+	echo "*** Starting to do the Large Lists"
+    #DICTS2=$(ls -1 dicts/Large_Lists/*)
+    DICTS2=$(find dicts/Large_Lists/ -type f -maxdepth 1)
     for d in $DICTS2; do
 	    $JTR \
 		  --config=${JTRconfigLM} \
 		  --session=LM_phase1 \
 		  --pot=LM_phase1 \
-		  --w:$d \
+		  --w:"$d" \
 		  --format=lm \
 		  $PASSWD
     done
-fi
 
 ### FOR BRUTEFORCING THE REST
-echo "hit CTRL-C after 20mins or whenever the passwords stop spewing"
-$JTR \
-    --config=$JTRconfigLM \
-    --session=LM_phase1 \
-    --pot=LM_phase1 \
-    --format=LM \
-    $PASSWD
-
+#	echo "*** Starting to do BRUTE FORCE"
+#    echo "*** Hit CTRL-C after 20mins or whenever the passwords stop spewing"
+#    $JTR \
+#	   --config=$JTRconfigLM \
+#	   --session=LM_phase1 \
+#	   --pot=LM_phase1 \
+#	   --format=LM \
+#	   $PASSWD
+	echo "*** Starting to do INCREMENTAL-ROCKYOULANMAN "
+    $JTR \
+	   --incremental=RockYouLanMan \
+	   --session=LM_phase1 \
+	   --pot=LM_phase1 \
+	   --format=LM \
+	   $PASSWD
+fi	#  end of if DEBUG
+exit 0
 ### extraction of cracked LM
-$JTR -format=lm -show $PASSWD | \
+echo "*** Extracting the cracked LM hashes"
+$JTR -format=lm -show -pot=LM_phase1 $PASSWD | \
 	grep -v "password hashes cracked" | \
 	sort -u | \
 	cut -d: -f2 > LMcracked
 
-### using that cracked LM list as wordlist, with different rules
+### using the cracked LM list as wordlist, with different rules
 $JTR \
 	--config=${JTRconfigNT} \
 	--session=NT_phase1 \
