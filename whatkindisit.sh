9f54649b92c50908fbfa3b402713d257632eb9af
Marcin Pohl <marcinpohl@gmail.com>
2011-08-02 07:12:42 -0400

    bunch of changes, added a script to determine which md5-gen it is

--- whatkindisit.sh
+++ whatkindisit.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-JOHN=~/hashes/crackall.git/john-1.7.8-jumbo-4
+INPUTFILE=$1
+BASEDIR=~/hashes/crackall.git
+JOHN=$BASEDIR/john-1.7.8-jumbo-4
 JTR=$JOHN/run/john
-SUBTYPES=$( $JTR passwords/paste52.txt --format=gen_md5 --subformat=list | cut -d' ' -f3 | cut -b9- | tr -d \) )
+DICTS=/data/dicts
+POTS=$BASEDIR/pots
 
-echo $SUBTYPES
+SUBTYPES=$( $JTR $BASEDIR/passwords/paste52.txt --format=gen_md5 --subformat=list | cut -d' ' -f3 | cut -b9- | tr -d \) )
+
+#echo $SUBTYPES
 
 for st in $SUBTYPES
 do
-	$JTR passwords/paste52.txt --format=md5-gen "--subformat=md5_gen($st)" --wordlist=$DICTS/Quick_Lists/rockyou.txt --mem-file-size=500
+	echo "*** WORKING on subtype" $st
+	$JTR $INPUTFILE --format=md5-gen "--subformat=md5_gen($st)" \
+		--wordlist=$DICTS/Quick_Lists/rockyou.txt --mem-file-size=500 
+		--pot=$POTS/whatsinit_$(basename ${INPUTFILE})_${st}
+
+	### this doesnt work because jtr doesnt take multi-dictionary inputs (but hashcat does, hint hint)
+	#$JTR $INPUTFILE --format=md5-gen "--subformat=md5_gen($st)" --wordlist=$DICTS/Really_Quick/* --mem-file-size=500
 done
